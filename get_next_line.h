51b3668ac65edbe7704f34da95f7209622447d64
김건우 <papawolf@kakao.com>
2020-11-04 19:14:20 +0900

    Add linked list

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,28 @@
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 19:17:09 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/29 21:54:21 by gunkim           ###   ########.fr       */
+/*   Updated: 2020/11/04 19:14:01 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
-# define BUFFER_SIZE 32
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2
+# endif
 
-int		get_next_line(int fd, char **line);
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+int					get_next_line(int fd, char **line);
 
 #endif
